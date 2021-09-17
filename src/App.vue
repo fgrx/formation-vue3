5028c20f7382201838b595f955cd188d31cce8d6
Editions les crocos <lestroiscigognes@gmail.com>
2021-09-17 16:48:01 +0200

    Chargement des ressources, navbar, boucles et conditions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,75 @@
 <template>
-  <div></div>
+  <va-navbar text-color="#73ef71" color="dark" class="mb-2">
+    <template #left>
+      <va-navbar-item class="title-site">DevWall</va-navbar-item>
+    </template>
+
+    <template #right>
+      <va-navbar-item></va-navbar-item>
+    </template>
+  </va-navbar>
+
+  <div class="container">
+    <h1 class="display-1">Bienvenue sur DevWall</h1>
+
+    <div class="row">
+      <div
+        v-for="ressource in ressources"
+        :key="ressource.id"
+        class="flex lg3 md4 sm12 pa-3"
+      >
+        <va-card>
+          <va-image
+            style="height: 220px"
+            :src="ressource.image || require('@/assets/default-image.png')"
+          />
+
+          <va-card-title
+            ><span v-if="ressource.isTop"><va-icon name="star" /> </span>
+            {{ ressource.title }}</va-card-title
+          >
+          <va-card-content>
+            {{ ressource.media }} ajouté(e) le
+            {{ ressource.date }}</va-card-content
+          >
+        </va-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import db from "@/data/db";
+
+import { ref } from "@vue/reactivity";
+
 export default {
   setup() {
-    return {};
+    const ressources = ref(db);
+
+    return {
+      ressources,
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+#app {
+  font-family: "Source Sans Pro", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+
+.container {
+  font-size: 16px;
+  line-height: 1.5;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 1185px;
+}
+
+.title-site {
+  font-size: 1.4em;
+  text-transform: uppercase;
+}
 </style>
